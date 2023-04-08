34790c2d40e558a1ce03b0bd59bb4e691d401fc1
Atikur <atikurrahman7223@gmail.com>
2023-04-08 12:57:46 +0600

    all books are rendered on ui

---
+++ src/components/Book.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Book = ({book}) => {
+    return (
+        <div className='relative overflow-hidden shadow-lg rounded hover:shadow-2xl transition duration-200 transform hover:-translate-y-2'>
+            <img src={book.image} alt="book cover" className='object-cover w-full h-56 md:h-64 xl:h-80' />
+
+            <div className='bg-black bg-opacity-75 opacity-0 hover:opacity-100 text-gray-300 px-6 py-4 absolute inset-0 transition-opacity duration-200 flex flex-col'>
+                <h4 className='mb-4 text-lg font-bold text-gray-100'>{book.title}</h4>
+                <br />
+                <p className='text-sm tracking-wide'>{book.subtitle}</p>
+                <br />
+                <p className='mt-auto font-semibold text-sm tracking-wide'>Price: {book.price}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Book from './Book';
 
 const Books = () => {
+    const {books} = useLoaderData();
+
     return (
-        <div>
-            BOOK pages
+        <div className='my-container'>
+            <div className='grid gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4'>
+                {books.map(book => <Book key={book.isbn13} book={book} />)}
+            </div>
         </div>
     );
 };
